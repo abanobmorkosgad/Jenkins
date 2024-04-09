6bdede8513571624e8845a8d09029d6d34aa05f8
Abanob Morkos <bibo@server.bibo.com>
2024-04-09 10:10:54 +0200

    update jenkins

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose'
-sudo mv docker-compose /usr/local/bin"
-sudo chmod +x /usr/local/bin/docker-compose"
-# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose"
-sudo docker-compose -f docker-compose.yaml up --detach"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
+sudo mv docker-compose /usr/local/bin
+sudo chmod +x /usr/local/bin/docker-compose
+# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo docker-compose -f docker-compose.yaml up --detach
