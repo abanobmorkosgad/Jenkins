0220b4d4fe07248590b03a9441fd1f8cecd8cfc8
Abanob Morkos <bibo@server.bibo.com>
2024-04-09 11:16:40 +0200

    update jenkins

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export IMAGE="abanobmorkos10/java-maven:1.1.12-25"
+export IMAGE=$1
 echo $IMAGE
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
 # sudo mv docker-compose /usr/local/bin
 # sudo chmod +x /usr/local/bin/docker-compose
 # sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo docker-compose -f docker-compose.yaml up --detach
+sudo IMAGE=$IMAGE docker-compose -f docker-compose.yaml up --detach
