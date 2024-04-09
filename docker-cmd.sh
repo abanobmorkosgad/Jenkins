59770729e62d613feb1b7ca221dbf410c2531e97
Abanob Morkos <bibo@server.bibo.com>
2024-04-09 10:58:40 +0200

    update jenkins

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export IMAGE=$1
+echo $IMAGE
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
 # sudo mv docker-compose /usr/local/bin
 # sudo chmod +x /usr/local/bin/docker-compose
